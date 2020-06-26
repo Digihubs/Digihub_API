fbfea1e62b2aae9fbb482b28cccd004deb73fe2f
Azim <azim@disystem.net>
2020-06-26 21:50:35 +0600

    Change ApplicationDBContext Class Name to DigiHubDBContext

--- DigiHub/DigiHub.Persistence/DigihubDBContextSeed.cs
+++ DigiHub/DigiHub.Persistence/DigihubDBContextSeed.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DigiHub.Persistence
 {
-    class ApplicationDBContext : DbContext
+    class DigiHubDBContextSeed
     {
     }
 }
