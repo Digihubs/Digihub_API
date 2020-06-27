88ad011e2e71136be167a77e864c0b48a7ec1e75
Azim <azim@disystem.net>
2020-06-27 23:05:45 +0600

    Add MappingProfilw and Exception Handling class

---
+++ DigiHub/DigiHub.Application/Common/Interfaces/IDigiHubDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DigiHub.Domain;
+
+namespace DigiHub.Application.Common.Interfaces
+{
+    public interface IDigiHubDBContext
+    {
+       
+    }
+}
